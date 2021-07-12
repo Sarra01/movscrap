f3bb94d23d32438480cfd184bf1eff9d13d79710
Sarra Merchaoui <sarra.merchaoui@supcom.tn>
2021-07-12 10:29:23 +0100

    initial commit

---
+++ movscrap-boot/src/main/java/com/example/demo/MovscrapApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovscrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovscrapApplication.class, args);
+	}
+
+}
